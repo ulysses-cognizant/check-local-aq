48cae60ad6ae965fa15be34a8927d8d163807966
will-j-richards <140435419+will-j-richards@users.noreply.github.com>
2023-10-10 11:56:10 +0100

    Initial prototype

--- app/routes.js
+++ app/routes.js
@@ -5,5 +5,15 @@
 
 const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
+const locationController = require('./controllers/location.js')
+const localAuthorityController = require('./controllers/local-authority.js')
 
 // Add your routes here
+
+
+// Location
+router.get('/location', locationController.get)
+
+// Local authority
+router.get('/local-authorities/', localAuthorityController.list)
+router.get('/local-authorities/:id', localAuthorityController.get)
