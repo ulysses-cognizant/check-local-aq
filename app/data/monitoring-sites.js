b539c5c27b4aeac02154bba928c12754d8317e0a
will-j-richards <140435419+will-j-richards@users.noreply.github.com>
2023-12-05 17:56:01 +0000

    Updates to monitoring sites table

--- app/data/monitoring-sites.js
+++ app/data/monitoring-sites.js
@@ -2,50 +2,50 @@ const monitoringSites = [
   {
       "site_name": "Leamington Spa",
       "distance": 1.35,
-      "site_type": "Information about this type of monitoring site in Birmingham",
+      "site_type": "This monitoring site is based in an urban area.",
       "pollutants": [
           {
               "type": "Ozone",
               "href": "/ozone",
               "measurement": 34.526,
-              "trend": "Steady",
+              "trend": "Rising",
               "band": "Low",
-              "aqi": 3,
-              "low_range": 0
+              "aqi": 2,
+              "low_range": "0 to 100"
           },
           {
               "type": "Nitrogen dioxide",
               "href": "/nitrogen-dioxide",
               "measurement": 27.349,
               "trend": "Steady",
-              "band": "High",
-              "aqi": 7,
-              "low_range": 0
+              "band": "Low",
+              "aqi": 1,
+              "low_range": "0 to 200"
           },
           {
             "type": "PM10",
             "href": "/particulate-matter10",
             "measurement": 11.800,
-            "trend": "Steady",
-            "band": "High",
-            "aqi": 7,
-            "low_range": 0
+            "trend": "Falling",
+            "band": "Low",
+            "aqi": 1,
+            "low_range": "0 to 50"
         },
         {
           "type": "PM2.5",
           "href": "/particulate-matter2",
           "measurement": 8.113,
-          "trend": "Steady",
-          "band": "High",
-          "aqi": 7,
-          "low_range": 0
+          "trend": "Falling",
+          "band": "Low",
+          "aqi": 1,
+          "low_range": "0 to 35"
       }
       ]
   },
   {
       "site_name": "Leamington Spa Rugby Road",
       "distance": 2.28,
-      "site_type": "Information about this type of monitoring site in Cardiff",
+      "site_type": "This monitoring site is based near vehicle traffic.",
       "pollutants": [
           {
               "type": "Nitrogen dioxide",
@@ -53,33 +53,33 @@ const monitoringSites = [
               "measurement": 21.611,
               "trend": "Steady",
               "band": "Low",
-              "aqi": 3,
-              "low_range": 0
+              "aqi": 1,
+              "low_range": "0 to 200"
           },
           {
             "type": "PM10",
             "href": "/particulate-matter10",
             "measurement": 	9.500,
-            "trend": "Steady",
-            "band": "High",
-            "aqi": 7,
-            "low_range": 0
+            "trend": "Rising",
+            "band": "Low",
+            "aqi": 1,
+            "low_range": "0 to 50"
         },
         {
           "type": "PM2.5",
           "href": "/particulate-matter2",
           "measurement": 6.226,
-          "trend": "Steady",
-          "band": "High",
-          "aqi": 7,
-          "low_range": 0
+          "trend": "Rising",
+          "band": "Low",
+          "aqi": 1,
+          "low_range": "0 to 35"
       }
       ]
   },
   {
       "site_name": "Coventry Binley Road",
       "distance": 14.61,
-      "site_type": "Information about this type of monitoring site in Newport",
+      "site_type": "This monitoring site is based near vehicle traffic.",
       "pollutants": [
           {
               "type": "Nitrogen dioxide",
@@ -87,17 +87,17 @@ const monitoringSites = [
               "measurement": 24.289,
               "trend": "Steady",
               "band": "Low",
-              "aqi": 3,
-              "low_range": 0
+              "aqi": 1,
+              "low_range": "0 to 200"
           },
           {
             "type": "PM10",
             "href": "/particulate-matter10",
             "measurement": 	13.527,
-            "trend": "Steady",
-            "band": "High",
-            "aqi": 7,
-            "low_range": 0
+            "trend": "Falling",
+            "band": "Low",
+            "aqi": 1,
+            "low_range": "0 to 50"
         }
       ]
   }
