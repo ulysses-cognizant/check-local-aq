5fb06fb95f4e6fe402d49b6b2f7851e5cf3c40d0
will-j-richards <140435419+will-j-richards@users.noreply.github.com>
2024-02-13 12:02:44 +0000

    CB3 Postcode

--- app/data/monitoring-sitesM27.js
+++ app/data/monitoring-sitesM27.js
@@ -4,28 +4,11 @@
 const monitoringSites = [
   
   {
-    "site_name": "Cambridge Roadside",
-    "site_id": "UKA00396",
-    "distance": 4.3,
-    "site_type": "traffic-site",
+    "site_name": "Salford Eccles",
+    "site_id": "UKA00339",
+    "distance": 1.8,
+    "site_type": "urban-site",
     "pollutants": [
-        {
-            "type": "nitrogen-dioxide",
-            "measurement": 219.436,
-            "trend": "Rising",
-            "band": "Moderate",
-            "aqi": 4,
-            "low_range": "0 to 200"
-        }
-    ]
-},
-
-  {
-      "site_name": "Wicken Fen",
-      "site_id": "UUKA00362",
-      "distance": 12.7,
-      "site_type": "rural-site",
-      "pollutants": [
         {
             "type": "ozone",
             "measurement": 39.914,
@@ -34,6 +17,47 @@ const monitoringSites = [
             "aqi": 2,
             "low_range": "0 to 100"
         },
+        {
+            "type": "nitrogen-dioxide",
+            "measurement": 219.436,
+            "trend": "Rising",
+            "band": "Moderate",
+            "aqi": 4,
+            "low_range": "0 to 200"
+        },
+        {
+            "type": "sulphur-dioxide",
+            "measurement": 220.436,
+            "trend": "Steady",
+            "band": "Moderate",
+            "aqi": 4,
+            "low_range": "0 to 200"
+        },
+        {
+            "type": "particulate-matter10",
+            "measurement": 8.113,
+            "trend": "Falling",
+            "band": "Low",
+            "aqi": 1,
+            "low_range": "0 to 35"
+        },
+        {
+            "type": "particulate-matter2",
+            "measurement": 37.2,
+            "trend": "Falling",
+            "band": "Moderate",
+            "aqi": 4,
+            "low_range": "0 to 35"
+    }
+    ]
+},
+
+  {
+      "site_name": "Bury Whitefield Roadside",
+      "site_id": "UKA00598",
+      "distance": 3.7,
+      "site_type": "traffic-site",
+      "pollutants": [
           {
               "type": "nitrogen-dioxide",
               "measurement": 150.289,
@@ -42,14 +66,6 @@ const monitoringSites = [
               "aqi": 3,
               "low_range": "0 to 200"
           },
-          {
-            "type": "sulphur-dioxide",
-            "measurement": 30.62,
-            "trend": "Steady",
-            "band": "Low",
-            "aqi": 4,
-            "low_range": "0 to 200"
-        },
         {
             "type": "particulate-matter10",
             "measurement": 7.623,
@@ -69,16 +85,32 @@ const monitoringSites = [
       ]
   },
   {
-    "site_name": "Sandy Roadside",
-    "site_id": "UKA00533",
-    "distance": 14.9,
-    "site_type": "traffic-site",
+    "site_name": "Manchester Piccadilly",
+    "site_id": "UKA00248",
+    "distance": 4.5,
+    "site_type": "urban-site",
     "pollutants": [
+        {
+            "type": "ozone",
+            "measurement": 39.914,
+            "trend": "Rising",
+            "band": "Low",
+            "aqi": 2,
+            "low_range": "0 to 100"
+        },
         {
             "type": "nitrogen-dioxide",
             "measurement": 220.436,
             "trend": "Falling",
             "band": "Moderate",
+            "aqi": 4,
+            "low_range": "0 to 200"
+        },
+        {
+            "type": "sulphur-dioxide",
+            "measurement": 30.62,
+            "trend": "Steady",
+            "band": "Low",
             "aqi": 4,
             "low_range": "0 to 200"
         },
